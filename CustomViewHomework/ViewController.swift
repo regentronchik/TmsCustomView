d29af8de6b87099b96eed1f988d9ed9a52f47461
regentronchik <59078332+regentronchik@users.noreply.github.com>
2023-11-20 20:37:24 +0300

    Changes in customView added

--- CustomViewHomework/ViewController.swift
+++ CustomViewHomework/ViewController.swift
@@ -18,8 +18,12 @@ class ViewController: UIViewController /*FirstViewDelegate*/ {
         view.addSubview(customView)
         customView.delegate = self
         customView.translatesAutoresizingMaskIntoConstraints = false
-        customView.centerXAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        customView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+//        customView.centerXAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+//        customView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        let heightConstraint = NSLayoutConstraint(item: customView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 250.0)
+        let widthConstraint = NSLayoutConstraint(item: customView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 370.0)
+        customView.addConstraints([heightConstraint, widthConstraint])
+        customView.clipsToBounds = true
         }
 //    func textFieldDidBeginEditing(text: String) {
 //        print("TextField did begin editing with text: \(text)")
